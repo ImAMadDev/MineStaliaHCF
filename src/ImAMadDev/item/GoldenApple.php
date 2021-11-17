41c326f607aebb88729e86e1a2f5deef811526a9
ImAMadDev <30436794+ImAMadDev@users.noreply.github.com>
2021-11-17 11:18:54 -0500

    Add files via upload

---
+++ src/ImAMadDev/item/GoldenApple.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ImAMadDev\item;
+
+use pocketmine\entity\effect\EffectInstance;
+use pocketmine\entity\effect\VanillaEffects;
+use pocketmine\item\Food;
+use pocketmine\item\ItemIdentifier;
+use pocketmine\item\ItemIds;
+
+class GoldenApple extends Food {
+	
+	public function __construct(){
+		parent::__construct(new ItemIdentifier(ItemIds::GOLDEN_APPLE, 0), "Golden Apple");
+	}
+	
+	public function requiresHunger() : bool {
+		return false;
+	}
+	
+	public function getSaturationRestore() : float {
+		return 9.6;
+	}
+	
+	public function getFoodRestore() : int {
+		return 6;
+	}
+	
+	public function getAdditionalEffects() : array {
+		return [
+			new EffectInstance(VanillaEffects::REGENERATION(), 100),
+			new EffectInstance(VanillaEffects::ABSORPTION(), 2400),
+			new EffectInstance(VanillaEffects::RESISTANCE(), 1400),
+		];
+	}
+}
---
+++ src/ImAMadDev/item/GoldenAppleEnchanted.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ImAMadDev\item;
+
+use pocketmine\entity\effect\{EffectInstance, VanillaEffects};
+use pocketmine\item\Food;
+use pocketmine\item\ItemIdentifier;
+use pocketmine\item\ItemIds;
+
+class GoldenAppleEnchanted extends GoldenApple {
+	
+	public function __construct(){
+		parent::__construct(new ItemIdentifier(ItemIds::ENCHANTED_GOLDEN_APPLE,0), "Enchanted Golden Apple");
+	}
+	
+	public function getAdditionalEffects() : array {
+		return [
+			new EffectInstance(VanillaEffects::REGENERATION(), 600, 4),
+			new EffectInstance(VanillaEffects::ABSORPTION(), 2400, 3),
+			new EffectInstance(VanillaEffects::RESISTANCE(), 6000),
+			new EffectInstance(VanillaEffects::FIRE_RESISTANCE(), 6000)
+		];
+	}
+}
